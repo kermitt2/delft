cfb76ef787e663dfa5301bd22d3f13b1f9094fa7
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2020-08-11 11:03:58 +0900

    fix tests

--- tests/sequenceLabelling/conftest.py
+++ tests/sequenceLabelling/conftest.py
@@ -7,8 +7,15 @@ from .test_data import TEST_DATA_PATH
 # derived from https://github.com/elifesciences/sciencebeam-trainer-delft/tree/develop/tests
 
 @pytest.fixture
-def sample_train_file():
+def sample_preprocessor1():
     return os.path.join(
         TEST_DATA_PATH,
-        'test-header.train'
+        'preprocessor.json'
+    )
+
+@pytest.fixture
+def sample_preprocessor2():
+    return os.path.join(
+        TEST_DATA_PATH,
+        'preprocessor2.json'
     )
