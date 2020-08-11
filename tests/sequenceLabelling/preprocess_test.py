2776e5b313329adcc2bd86578cc4582e1e465df4
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2020-08-11 10:58:27 +0900

    fixing tests

--- tests/sequenceLabelling/preprocess_test.py
+++ tests/sequenceLabelling/preprocess_test.py
@@ -4,11 +4,11 @@ from delft.sequenceLabelling.preprocess import WordPreprocessor
 class TestWordPreprocessor:
 
     def test_load_example(self):
-        p = WordPreprocessor.load('data/preprocessor.json')
+        p = WordPreprocessor.load('sequenceLabelling/test_data/preprocessor.json')
 
         assert len(p.vocab_char) == 70
 
     def test_load_withUmmappedVariable_shouldIgnore(self):
-        p = WordPreprocessor.load('data/preprocessor2.json')
+        p = WordPreprocessor.load('sequenceLabelling/test_data/preprocessor2.json')
 
         assert len(p.vocab_char) == 70
