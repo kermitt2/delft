dd086eb5b22a3061f370140ac46d59b13fc206a9
Patrice Lopez <patrice.lopez@science-miner.com>
2022-03-04 20:07:13 +0100

    avoid multiprocessing when training an architecture with transformer

--- delft/sequenceLabelling/trainer.py
+++ delft/sequenceLabelling/trainer.py
@@ -174,7 +174,7 @@ class Trainer(object):
         nb_workers = 6
         multiprocessing = self.training_config.multiprocessing
         # multiple workers should work with transformer layers, but not with ELMo due to GPU memory limit (with GTX 1080Ti 11GB)
-        if self.embeddings and self.embeddings.use_ELMo:
+        if self.model_config.transformer != None or (self.embeddings and self.embeddings.use_ELMo):
             # worker at 0 means the training will be executed in the main thread
             nb_workers = 0 
             multiprocessing = False
